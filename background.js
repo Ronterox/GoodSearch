02d3ef6d93c6e78511ed08dda185f09f36e8bbc1
Ricardo <ricarditomontserrat@gmail.com>
2023-02-09 23:28:26 +0100

    Functional and buildable, advance search 2.0

--- background.js
+++ background.js
@@ -1,4 +1,33 @@
-browser.omnibox.setDefaultSuggestion({ description: "Query will be search across multiple search engines." });
+const MODIFIERS = ['"', "-", "+", "inurl:", "filetype:", "site:"];
+browser.omnibox.setDefaultSuggestion({ description: "Move with \"TAB/SHIFT+TAB\", select with \"SPACEBAR\"" });
+browser.omnibox.onInputChanged.addListener((text, suggest) => {
+	const dotPriority = (str) => (str.includes(".") ? str.length * 3 : str.length);
+	const longerPriority = (a, b) => dotPriority(b) - dotPriority(a);
+
+	let suggestions = [];
+	for (let word of text.split(" ").sort(longerPriority)) {
+		const createSuggestion = (actText, prefix, suffix = "") => {
+			let wordQuery = prefix == "filetype:" ? word.substring(word.indexOf(".") + 1) : word;
+			let words = text.split(" ");
+			words[words.indexOf(word)] = prefix + wordQuery + suffix;
+			suggestions.push({
+				content: words.join(" "),
+				description: `Must ${actText} "${word}"`,
+			});
+		};
+
+		if (word.length < 2 || MODIFIERS.some((mod) => word.includes(mod))) continue;
+		if (word.includes(".")) {
+			createSuggestion("be in URL ", "inurl:");
+			createSuggestion("be FILETYPE ", "filetype:");
+			createSuggestion("be SITE ", "site:");
+		}
+		createSuggestion("SHOW ", '"', '"');
+		createSuggestion("NOT show ", "-");
+		createSuggestion("show MORE ", "+");
+	}
+	suggest(suggestions);
+});
 
 let lastSearch = "";
 async function search(text, engineName, tabId = undefined) {
@@ -7,7 +36,7 @@ async function search(text, engineName, tabId = undefined) {
 			.query({})
 			.then((tabs) => {
 				for (let tab of tabs) {
-					if (tab.url.includes(engineName.split()[0].toLowerCase()) && tab.title.includes(lastSearch)) {
+					if (tab.url.includes(engineName.split(" ")[0].toLowerCase()) && tab.title.includes(lastSearch)) {
 						return tab.id;
 					}
 				}
